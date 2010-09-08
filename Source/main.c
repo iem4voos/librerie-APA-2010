9dc78e28182fa9c3be214e085715ba677dcc7d75
Mic <your@email.com>
2010-09-08 21:18:49 +0200

    secondo commento

---
+++ Source/main.c
@@ -0,0 +1,12 @@
+//  main.c librerie-APA-2010  Created by mic on 08/09/10.
+
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[]) {
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
