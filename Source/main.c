7f087415e0d7c2274fb0c56deac99260bf2c3731
iem4voos <iem4voos@gmail.com>
2010-10-25 17:01:00 +0200

    Initial Commit

---
+++ Source/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  MicLibApa2010
+//
+//  Created by mic on 25/10/10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[]) {
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
